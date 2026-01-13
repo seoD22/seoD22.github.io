0f23de3d65dcf2a56b4dbd0084964ed81ac8824d
seohee_Yun <114120552+seoD22@users.noreply.github.com>
2026-01-13 16:55:06 +0900

    aside

--- js/default.js
+++ js/default.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
 $(function() {
   const elementsToLoad = {
     "#header": "/includes/header.html",
@@ -6,6 +8,7 @@ $(function() {
 
   $.each(elementsToLoad, function(selector, url) {
     $(selector).load(url, function(response, status, xhr) {
+      quickMenu();
       if (status === "error") {
         console.error(`Failed to load ${url}: ${xhr.status} ${xhr.statusText}`);
       } 
@@ -26,5 +29,37 @@ $(function() {
 function initNavigation() {
   $('.btn-menu').on('click', function() {
     $('.gnb').toggleClass('active');
+  });
+}
+
+// 퀵메뉴
+function quickMenu() {
+  // 1. 섹션 이동
+  $('aside a').on('click', function(e) {
+    e.preventDefault();
+    const target = $(this).attr('href');
+    gsap.to(window, {
+      duration: 0.5, 
+      scrollTo: { y: target, offsetY: 80 },
+      ease: "power2.inOut"
+    });
+  });
+
+  // 2. 섹션 활성화
+  const menuLinks = $('aside li');
+  const sections = $('section');
+
+  sections.each(function(i, section) {
+    ScrollTrigger.create({
+      trigger: section,
+      start: "top 20%",
+      end: "bottom 20%",
+      onToggle: self => {
+        if (self.isActive) {
+          menuLinks.removeClass('active');
+          menuLinks.eq(i).addClass('active');
+        }
+      }
+    });
   });
 }
