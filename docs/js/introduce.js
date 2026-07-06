d5db944319351abfde64b417e2ceee85cafa60cd
SeoHee <cobbt258@naver.com>
2026-07-06 21:00:23 +0900

    집에서 작업

--- docs/js/introduce.js
+++ docs/js/introduce.js
@@ -111,253 +111,6 @@ function asideBtnEvent() {
 //   });
 // }
 
-// 바닥에 오면 사이트 버튼 없어짐
-function asideBtnNone() {
-  var scrollTop = $(window).scrollTop();
-  var innerHeight = $(window).innerHeight();
-  var scrollHeight = $(document).height();
-
-  if (scrollTop + innerHeight >= scrollHeight) {
-      $('aside').addClass('end');
-  } else {
-      $('aside').removeClass('end');
-  }
-}
-
-// 포트폴리오 스와이퍼
-function portfolioSwiper() {
-  const asideBtn = document.querySelector('aside');
-  // html 클래스 유무에 따라 autoplay 실행
-  asideBtn.addEventListener('click', (e) => {
-    if ($('html').hasClass('show')) {
-      swiper.autoplay.start();
-    } else {
-      swiper.autoplay.stop();
-    }
-  })
-
-  var swiper = new Swiper(".port_swiper", {
-    direction: "vertical",
-    slidesPerView: 2,
-    spaceBetween: 0,
-    centeredSlides: true,
-    loop: true,
-    mousewheel: true,
-    speed: 600,
-    autoplay: false,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    breakpoints: {
-      768: {
-        slidesPerView: 1.2,
-        spaceBetween: 30,
-      },
-      1600: {
-        slidesPerView: 1.4,
-        spaceBetween: 30,
-      },
-      1900: {
-        slidesPerView: 1.7,
-        spaceBetween: 30,
-      },
-    },
-    on: {
-      slideChange: function() {
-        // 페이징
-        if (window.innerWidth >= 769) {
-          $('.portfolio .pagi .lineWrap span').css({
-            'left': '0',
-            'top': `${(this.realIndex) * 20}%`
-          });
-      } else {
-        $('.portfolio .pagi .lineWrap span').css({
-          'top': '0',
-          'left': `${(this.realIndex) * 20}%`
-        });
-      }
-      },
-
-      slideChangeTransitionEnd: function () {
-        if(this.realIndex == 0){
-          v1motion.play();
-
-          // 초기화
-          v2motion.progress(0);
-          v2motion.pause();
-
-          v5motion.progress(0);
-          v5motion.pause();
-          
-        }else if(this.realIndex == 1){
-            v2motion.play();
-
-            // 초기화
-            v1motion.progress(0);
-            v1motion.pause();
-
-            v3motion.progress(0);
-            v3motion.pause();
-
-        }else if(this.realIndex == 2){
-          v3motion.play();
-
-          // 초기화
-          v2motion.progress(0);
-          v2motion.pause();
-
-          v4motion.progress(0);
-          v4motion.pause();
-
-        }else if(this.realIndex == 3){
-          v4motion.play();
-
-          // 초기화
-          v5motion.progress(0);
-          v5motion.pause();
-
-          v3motion.progress(0);
-          v3motion.pause();
-        }else if(this.realIndex == 4){
-          v5motion.play();
-
-          // 초기화
-          v1motion.progress(0);
-          v1motion.pause();
-
-          v4motion.progress(0);
-          v4motion.pause();
-        }
-      },
-    }
-  });
-
-  // 스와이퍼 anime
-  let v1Tit = '.portfolio .swiper-slide._01 .port_des h3';
-  let v1Btn = '.portfolio .swiper-slide._01 .port_des .btn';
-  let v1top = '.portfolio .swiper-slide._01 .top';
-  let v1tag = '.portfolio .swiper-slide._01 .tag_box';
-  let v1bar = '.portfolio .swiper-slide._01 .top .bar';
-
-  gsap.set(v1Tit, { opacity: 0, xPercent: -20 });
-  gsap.set(v1Btn, { opacity: 0, xPercent: -20 });
-  gsap.set(v1top, { opacity: 0 });
-  gsap.set(v1tag, { opacity: 0 });
-  gsap.set(v1bar, { width: 0 });
-
-  let v1motion = gsap.timeline();
-
-  v1motion
-    .addLabel('label_01')
-    .to(v1top, { opacity: 1, duration: .8}, "label_01" )
-    .to(v1tag, { opacity: 1, duration: .8}, "label_01" )
-    .to(v1bar, { width: '100%', delay: .2}, "label_01" )
-    .to(v1Tit, { opacity: 1, xPercent: 0, duration: .8, delay: .2}, "label_01")
-    .to(v1Btn, { opacity: 1, xPercent: 0, duration: .8}, "-=50%" )
-
-
-  // visual2
-  let v2Tit = '.portfolio .swiper-slide._02 .port_des h3';
-  let v2Btn = '.portfolio .swiper-slide._02 .port_des .btn';
-  let v2top = '.portfolio .swiper-slide._02 .top';
-  let v2tag = '.portfolio .swiper-slide._02 .tag_box';
-  let v2bar = '.portfolio .swiper-slide._02 .top .bar';
-
-  gsap.set(v2Tit, { opacity: 0, xPercent: -20 });
-  gsap.set(v2Btn, { opacity: 0, xPercent: -20 });
-  gsap.set(v2top, { opacity: 0 });
-  gsap.set(v2tag, { opacity: 0 });
-  gsap.set(v2bar, { width: 0 });
-
-  let v2motion = gsap.timeline();
-
-  v2motion
-    .addLabel('label_01')
-    .to(v2top, { opacity: 1, duration: .8}, "label_01" )
-    .to(v2tag, { opacity: 1, duration: .8}, "label_01" )
-    .to(v2bar, { width: '100%', delay: .2}, "label_01" )
-    .to(v2Tit, { opacity: 1, xPercent: 0, duration: .8, delay: .2}, "label_01")
-    .to(v2Btn, { opacity: 1, xPercent: 0, duration: .8}, "-=50%" )
-
-  v2motion.pause();
-
-  // visual3
-  let v3Tit = '.portfolio .swiper-slide._03 .port_des h3';
-  let v3Btn = '.portfolio .swiper-slide._03 .port_des .btn';
-  let v3top = '.portfolio .swiper-slide._03 .top';
-  let v3tag = '.portfolio .swiper-slide._03 .tag_box';
-  let v3bar = '.portfolio .swiper-slide._03 .top .bar';
-
-  gsap.set(v3Tit, { opacity: 0, xPercent: -20 });
-  gsap.set(v3Btn, { opacity: 0, xPercent: -20 });
-  gsap.set(v3top, { opacity: 0 });
-  gsap.set(v3tag, { opacity: 0 });
-  gsap.set(v3bar, { width: 0 });
-
-  let v3motion = gsap.timeline();
-
-  v3motion
-    .addLabel('label_01')
-    .to(v3top, { opacity: 1, duration: .8}, "label_01" )
-    .to(v3tag, { opacity: 1, duration: .8}, "label_01" )
-    .to(v3bar, { width: '100%', delay: .2}, "label_01" )
-    .to(v3Tit, { opacity: 1, xPercent: 0, duration: .8, delay: .2}, "label_01")
-    .to(v3Btn, { opacity: 1, xPercent: 0, duration: .8}, "-=50%" )
-
-  v3motion.pause();
-
-  // visual4
-  let v4Tit = '.portfolio .swiper-slide._04 .port_des h3';
-  let v4Btn = '.portfolio .swiper-slide._04 .port_des .btn';
-  let v4top = '.portfolio .swiper-slide._04 .top';
-  let v4tag = '.portfolio .swiper-slide._04 .tag_box';
-  let v4bar = '.portfolio .swiper-slide._04 .top .bar';
-
-  gsap.set(v4Tit, { opacity: 0, xPercent: -20 });
-  gsap.set(v4Btn, { opacity: 0, xPercent: -20 });
-  gsap.set(v4top, { opacity: 0 });
-  gsap.set(v4tag, { opacity: 0 });
-  gsap.set(v4bar, { width: 0 });
-
-  let v4motion = gsap.timeline();
-
-  v4motion
-    .addLabel('label_01')
-    .to(v4top, { opacity: 1, duration: .8}, "label_01" )
-    .to(v4tag, { opacity: 1, duration: .8}, "label_01" )
-    .to(v4bar, { width: '80%', delay: .2}, "label_01" )
-    .to(v4Tit, { opacity: 1, xPercent: 0, duration: .8, delay: .2}, "label_01")
-    .to(v4Btn, { opacity: 1, xPercent: 0, duration: .8}, "-=50%" )
-
-  v4motion.pause();
-
-  // visual5
-  let v5Tit = '.portfolio .swiper-slide._05 .port_des h3';
-  let v5Btn = '.portfolio .swiper-slide._05 .port_des .btn';
-  let v5top = '.portfolio .swiper-slide._05 .top';
-  let v5tag = '.portfolio .swiper-slide._05 .tag_box';
-  let v5bar = '.portfolio .swiper-slide._05 .top .bar';
-
-  gsap.set(v5Tit, { opacity: 0, xPercent: -20 });
-  gsap.set(v5Btn, { opacity: 0, xPercent: -20 });
-  gsap.set(v5top, { opacity: 0 });
-  gsap.set(v5tag, { opacity: 0 });
-  gsap.set(v5bar, { width: 0 });
-
-  let v5motion = gsap.timeline();
-
-  v5motion
-    .addLabel('label_01')
-    .to(v5top, { opacity: 1, duration: .8}, "label_01" )
-    .to(v5tag, { opacity: 1, duration: .8}, "label_01" )
-    .to(v5bar, { width: '50%', delay: .2}, "label_01" )
-    .to(v5Tit, { opacity: 1, xPercent: 0, duration: .8, delay: .2}, "label_01")
-    .to(v5Btn, { opacity: 1, xPercent: 0, duration: .8}, "-=50%" )
-
-  v5motion.pause();
-}
-
 // gsap anime
 function visual() {
   const sectionVisual = document.querySelector('section._visual');
@@ -435,51 +188,25 @@ function visual() {
       duration: 0.6,
       ease: "power1.out"
     }, "label_02");
-  }
+}
+  
 
 function section02_toggle() {
   $('.com_list').removeClass('view');
   $('.com_list .desc_box').hide();
 
-  $('.com_list .work_title button')
-    .attr('aria-expanded', 'false')
-    .text('펼치기');
-
-  $('.com_list .work_title button').on('click', function() {
-    const $btn = $(this);
-    const $list = $btn.closest('.com_list');
-    const $descBox = $list.find('.desc_box');
-    
-    const isExpanded = $btn.attr('aria-expanded') === 'true';
-
-    if (!isExpanded) {
-      $list.addClass('view');
-      $descBox.stop().slideDown(300);
-      
-      $btn.attr('aria-expanded', 'true').text('접기');
-    } else {
-      $descBox.stop().slideUp(300, function() {
-        $list.removeClass('view');
-      });
-    
-      $btn.attr('aria-expanded', 'false').text('펼치기');
-    }
-  });
-}
-
-function section03_toggle() {
-  $('section._03 li').removeClass('view');
-  $('section._03 li ._desc').hide();
-  
-  $('section._03 li ._title button')
+  $('.com_list .btn_toggle')
     .attr('aria-expanded', 'false')
     .find('.sr-only').text('열기');
 
-  $('section._03 li ._title button').on('click', function() {
+  $('.com_list .btn_toggle').off('click').on('click', function(e) {
+    e.preventDefault();
+
     const $btn = $(this);
-    const $list = $btn.closest('section._03 li');
-    const $descBox = $list.find('._desc');
+    const $list = $btn.closest('.com_list'); 
+    const $descBox = $list.find('.desc_box'); 
     const $srText = $btn.find('.sr-only');
+    
     const isExpanded = $btn.attr('aria-expanded') === 'true';
 
     if (!isExpanded) {
@@ -487,12 +214,90 @@ function section03_toggle() {
       $descBox.stop().slideDown(300);
       
       $btn.attr('aria-expanded', 'true');
-      $srText.text('닫기');
+      $srText.text('닫기'); 
     } else {
       $descBox.stop().slideUp(300, function() {
         $list.removeClass('view');
       });
-      
+    
+      $btn.attr('aria-expanded', 'false');
+      $srText.text('열기');
+    }
+  });
+}
+
+function section03_toggle() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  $('section._03 li').removeClass('view').removeAttr('data-user-opened');
+  $('section._03 li ._desc').hide();
+  
+  $('section._03 li ._title button')
+    .attr('aria-expanded', 'false')
+    .find('.sr-only').text('열기');
+
+  $('section._03 li').each(function(index, element) {
+    const $list = $(element);
+    const $btn = $list.find('._title button');
+    const $descBox = $list.find('._desc');
+    const $srText = $btn.find('.sr-only');
+
+    ScrollTrigger.create({
+      trigger: element,
+      start: 'top 50%',
+      end: 'bottom 50%',
+      onEnter: function() {
+        if ($list.attr('data-user-opened') === 'true') return;
+        $list.addClass('view');
+        $descBox.stop().slideDown(300);
+        $btn.attr('aria-expanded', 'true');
+        $srText.text('닫기');
+      },
+      onLeave: function() {
+        if ($list.attr('data-user-opened') === 'true') return;
+        $descBox.stop().slideUp(300, function() {
+          $list.removeClass('view');
+        });
+        $btn.attr('aria-expanded', 'false');
+        $srText.text('열기');
+      },
+      onEnterBack: function() {
+        if ($list.attr('data-user-opened') === 'true') return;
+        $list.addClass('view');
+        $descBox.stop().slideDown(300);
+        $btn.attr('aria-expanded', 'true');
+        $srText.text('닫기');
+      },
+      onLeaveBack: function() {
+        if ($list.attr('data-user-opened') === 'true') return;
+        $descBox.stop().slideUp(300, function() {
+          $list.removeClass('view');
+        });
+        $btn.attr('aria-expanded', 'false');
+        $srText.text('열기');
+      }
+    });
+  });
+
+  $('section._03 li ._title button').off('click').on('click', function(e) {
+    e.preventDefault();
+    
+    const $btn = $(this);
+    const $list = $btn.closest('section._03 li');
+    const $descBox = $list.find('._desc');
+    const $srText = $btn.find('.sr-only');
+    const isExpanded = $btn.attr('aria-expanded') === 'true';
+
+    if (!isExpanded) {
+      $list.addClass('view').attr('data-user-opened', 'true');
+      $descBox.stop().slideDown(300);
+      $btn.attr('aria-expanded', 'true');
+      $srText.text('닫기');
+    } else {
+      $list.removeAttr('data-user-opened');
+      $descBox.stop().slideUp(300, function() {
+        $list.removeClass('view');
+      });
       $btn.attr('aria-expanded', 'false');
       $srText.text('열기');
     }
